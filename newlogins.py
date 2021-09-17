6f79635013e72781063d8330e73d54b27977240d
I1obody <90924529+I1obody@users.noreply.github.com>
2021-09-17 20:42:24 +0300

    Update newlogins.py

--- newlogins.py
+++ newlogins.py
@@ -2,16 +2,16 @@ import random
 import string
 
 
-def generate_alphanum_random_string(length):
+def MailNamesAndPassRandomiser(length):
     letters_and_digits = string.ascii_letters + string.digits
     rand_string = ''.join(random.sample(letters_and_digits, length))
     rang_string = ''.join(random.sample(letters_and_digits, length))
-    path = '/Users/I1obody/Desktop/random_logins_and_pass/random_logins_and_pass.txt'
+    path = 'random_logins_and_pass.txt'
     random_logins_and_pass = open(path,'a')
     title = 'login: '
     titlle = ' password: '
-    gmail =  '@outlook.com '
+    gmail =  '@outlook.com ' #u can change it on smt like  @gmail.com and etc
     random_logins_and_pass.write('\n' + title + rand_string + gmail + titlle + rang_string)
     random_logins_and_pass.close()
 
-generate_alphanum_random_string(10)
+MailNamesAndPassRandomiser(10) #change ten on any other number for switch length
